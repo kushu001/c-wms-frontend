386bbe619da4fb03f3db114b5fd44dce0a076e88
kushu001 <yj_tao123@sina.com>
2023-02-02 17:26:03 +0800

    feat(material.vue): 物料信息模块

--- src/views/basic/material/index.vue
+++ src/views/basic/material/index.vue
@@ -1,3 +1,47 @@
 <template>
-  <el-button type="primary">测试</el-button>
+  <cdp-table :table-config="tableConfig" />
 </template>
+<script>
+import CdpTable from '@/components/cdp-ui/cdp-template/cdp-table'
+
+export default {
+  components: {
+    CdpTable
+  },
+  data() {
+    return {
+      tableConfig: {
+        title: '物料信息',
+        url: '/api/v1/material',
+        columns: [
+          {
+            name: 'code',
+            type: 'input',
+            label: '物料编码',
+            formConfig: {
+              rules: [{ required: true, message: '物料编码不能为空' }]
+            }
+          },
+          {
+            name: 'name',
+            formConfig: {
+              rules: [{ required: true, message: '物料名称不能为空' }]
+            },
+            label: '物料名称'
+          },
+          {
+            name: 'spec',
+            type: 'input',
+            label: '规格'
+          },
+          {
+            name: 'model',
+            type: 'input',
+            label: '型号'
+          }
+        ]
+      }
+    }
+  }
+}
+</script>
